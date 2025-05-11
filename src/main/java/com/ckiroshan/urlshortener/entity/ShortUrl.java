f301b5baa619eaef25618d95df3b496cd176b130
ckiroshan <ckiroshan@gmail.com>
2025-05-11 10:07:12 +0530

    feat: Add ShortUrl entity

---
+++ src/main/java/com/ckiroshan/urlshortener/entity/ShortUrl.java
@@ -0,0 +1,20 @@
+package com.ckiroshan.urlshortener.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
+public class ShortUrl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false, length = 10)
+    private String shortCode;
+    @Column(unique = false, columnDefinition = "TEXT")
+    private String originalUrl;
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
