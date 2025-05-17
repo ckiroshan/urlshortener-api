506bd08cad3f3149553634dc0ee5ec32f60b7646
ckiroshan <ckiroshan@gmail.com>
2025-05-17 14:38:02 +0530

    feat: Add LoginRequest DTO for user authentication

---
+++ src/main/java/com/ckiroshan/urlshortener/user/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.ckiroshan.urlshortener.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class LoginRequest {
+    @NotBlank // Validation: ensures the field is not null or empty
+    @Email // Validation: ensures the string is in valid email format
+    private String email;
+    @NotBlank @Size(min = 8) // Validation: password must be at least 8 characters
+    private String password;
+}
