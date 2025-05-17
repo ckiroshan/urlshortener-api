131c051bdeef3766dc57d1096e38e519b118309f
ckiroshan <ckiroshan@gmail.com>
2025-05-17 14:56:34 +0530

    feat: Enhance UserMapper with AuthResponse conversion

--- src/main/java/com/ckiroshan/urlshortener/user/mapper/UserMapper.java
+++ src/main/java/com/ckiroshan/urlshortener/user/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.ckiroshan.urlshortener.user.mapper;
 
+import com.ckiroshan.urlshortener.user.dto.AuthResponse;
 import com.ckiroshan.urlshortener.user.dto.RegisterRequest;
 import com.ckiroshan.urlshortener.user.entity.User;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,15 @@ public class UserMapper {
                 .email(request.getEmail())
                 .password(request.getPassword()) // before encoding password
                 .role(request.getRole())
+                .build();
+    }
+
+    // Converts User entity and JWT token to AuthResponse DTO
+    public AuthResponse authDTOtoAuthResponse(User user, String token) {
+        return AuthResponse.builder()
+                .token(token)
+                .email(user.getEmail())
+                .role(user.getRole())
                 .build();
     }
 }
