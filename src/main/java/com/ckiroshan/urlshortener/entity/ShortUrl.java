ec2b0b646b84d37b3dd00dc4d4fa043708ae9c9c
ckiroshan <ckiroshan@gmail.com>
2025-05-18 09:14:05 +0530

    feat: Establish many-to-one relationship between ShortUrl & User

--- src/main/java/com/ckiroshan/urlshortener/entity/ShortUrl.java
+++ src/main/java/com/ckiroshan/urlshortener/entity/ShortUrl.java
@@ -1,5 +1,6 @@
 package com.ckiroshan.urlshortener.entity;
 
+import com.ckiroshan.urlshortener.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -7,16 +8,19 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-@Builder
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+
 public class ShortUrl {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increments the ID
     private Long id;
-    @Column(unique = true, nullable = false, length = 10)
+    @Column(unique = true, nullable = false, length = 10) // Unique short code
     private String shortCode;
-    @Column(unique = false, columnDefinition = "TEXT")
+    @Column(unique = false, columnDefinition = "TEXT") // Original long URL, stored as text
     private String originalUrl;
-    @CreationTimestamp
-    private LocalDateTime createdAt = LocalDateTime.now();
+    @CreationTimestamp // Timestamp when the entity is created
+    private LocalDateTime createdAt = LocalDateTime.now(); // Default value is local time
+    @ManyToOne(fetch = FetchType.LAZY) // Many ShortUrls can belong to one User
+    @JoinColumn(name = "user_id", nullable = false) // Foreign key column in the DB
+    private User user;
 }
