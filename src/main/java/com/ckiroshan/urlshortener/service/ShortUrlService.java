7ee2068eeb48ee86d187ab99332aeb4e89c57d9d
ckiroshan <ckiroshan@gmail.com>
2025-05-20 09:24:25 +0530

    feat: Add getAnalytics method to ShortUrlService

--- src/main/java/com/ckiroshan/urlshortener/service/ShortUrlService.java
+++ src/main/java/com/ckiroshan/urlshortener/service/ShortUrlService.java
@@ -1,5 +1,6 @@
 package com.ckiroshan.urlshortener.service;
 
+import com.ckiroshan.urlshortener.analytics.dto.AnalyticsResponse;
 import com.ckiroshan.urlshortener.dto.ShortUrlRequest;
 import com.ckiroshan.urlshortener.dto.ShortUrlResponse;
 
@@ -10,4 +11,6 @@ public interface ShortUrlService {
     String getOriginalUrl(String shortCode);
     // Deletes an existing short code
     void deleteShortUrl(String shortCode);
+    // Retrieves analytics data for a given short code & user's email
+    AnalyticsResponse getAnalytics(String shortcode, String userEmail);
 }
