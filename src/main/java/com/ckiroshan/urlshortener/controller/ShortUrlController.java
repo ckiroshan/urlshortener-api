1fc18671fa8da195771d53358b8927a7109aba6f
ckiroshan <ckiroshan@gmail.com>
2025-05-28 10:57:41 +0530

    feat: Add short URL deletion endpoint & adjust base path on ShortUrlController

--- src/main/java/com/ckiroshan/urlshortener/controller/ShortUrlController.java
+++ src/main/java/com/ckiroshan/urlshortener/controller/ShortUrlController.java
@@ -8,22 +8,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/shorten") // Base path for all URL shortening endpoints
+@RequestMapping("/api") // Base path for all URL shortening endpoints
 @RequiredArgsConstructor // Generates constructor for final fields
 public class ShortUrlController {
     private final ShortUrlService shortUrlService;
 
-    @PostMapping // Endpoint to create a shortened URL
+    @PostMapping("/shorten") // Endpoint to create a shortened URL
     public ResponseEntity<ShortUrlResponse> createShortUrl(
             @Valid @RequestBody ShortUrlRequest shortUrlRequest, // Validates and binds request body to DTO
             @AuthenticationPrincipal UserDetails userDetails) { // Injects authenticated user
         // Returns a response containing the shortened URL & related info
         return ResponseEntity.ok(shortUrlService.createShortUrl(shortUrlRequest, userDetails.getUsername()));
+    }
+
+    // Endpoint to delete a shortened URL
+    @DeleteMapping("/urls/{shortCode}")
+    public ResponseEntity<Void> deleteUrl(
+            @PathVariable String shortCode,  // Short code of the URL
+            @AuthenticationPrincipal UserDetails userDetails) { // Injects authenticated user
+        shortUrlService.deleteShortUrl(shortCode, userDetails.getUsername());
+        // Returns response 204 No Content on successful delete
+        return ResponseEntity.noContent().build();
     }
 }
